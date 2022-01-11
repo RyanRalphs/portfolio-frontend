b1a25cabed66cdba592b484f5c8204cd900ab6e8
Ryan <ryaaan_@live.co.uk>
2022-01-11 11:41:00 +0000

    Set up react, made dir structure for components and started on profile component

---
+++ src/Portfolio-Container/Home/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="cols">
+                        <a href="https://www.linkedin.com/in/ryan-ralphs-0465a7169/">
+                            <i className="fa fa-linkedin-square"></i>                       
+                                 </a>
+                        <a href="https://github.com/ryanralphs">
+                            <i className="fa fa-github-square"></i>                       
+                                 </a>
+                        <a href="https://twitter.com/ryanjralphs">
+                            <i className="fa fa-twitter-square"></i>                       
+                                 </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
